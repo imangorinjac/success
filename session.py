bd67301cc55e38fca688d17be9c31e1b3bafa05c
imangorinjac <iman.gorinjac@nethletic.com>
2021-10-28 23:31:10 +0200

    retrieve_user_input

--- session.py
+++ session.py
@@ -9,20 +9,29 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SECRET_KEY"] = "random string"
 db = SQLAlchemy(app)
 
-class Person(db.Model):
-    __table_args__ = {"extend_existing": True}
+
+class Folder(db.Model):
+
+    __tablename__ = "folder"
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(50))
+    files = db.relationship("File", backref="files")
 
-    def __init__(self, username, password):
+    def __repr__(self):
+        return f"Folder({self.name})"
 
-        self.username = username
-        self.password = password
+
+class File(db.Model):
+
+    __tablename__ = "file"
+
+    id = db.Column(db.Integer, primary_key=True)
+    folder_id = db.Column(db.Integer, db.ForeignKey("folder.id"))
+    name = db.Column(db.String(50))
+    size = db.Column(db.Integer)
 
 db.create_all()
 
-
-person = Person(username='susan', email='susan@example.com')
-db.session.add(person)
-db.session.commit()
+def __repr__(self):
+        return f"File({self.folder_id}, {self.name}, {self.size})"
